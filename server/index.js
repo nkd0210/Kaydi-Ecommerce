9c70302275ec60499c8ecae0c26894cfd868f15c
Nguyễn Kim Dũng <kimdungvn52@gmail.com>
2024-10-20 21:50:52 +0700

    chore: add vercel.json for deployment

--- server/index.js
+++ server/index.js
@@ -30,7 +30,12 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json()); // allow json as the input of the BE
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
